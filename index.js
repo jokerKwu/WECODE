57e8873fe20b841730dd02ee9fe694ec5db1e011
joker <pkjhj485@naver.com>
2021-02-12 19:51:52 +0900

    dd

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(e) {
+    var i = 0;
+    var len = $('.slide .textSlide .slide-txt').length;
+    var timeLoop;
+        timeLoop = setInterval(function(){             
+            $('.slide .textSlide .slide-txt').eq(i).css("display","none"); // hide all the li elements
+            i = i + 1;
+            
+            if(i >= len){
+                 i = 0;
+            }
+            $('.slide .textSlide .slide-txt').eq(i).css("display","block"); // show the current li
+         }, 2000);
+});
